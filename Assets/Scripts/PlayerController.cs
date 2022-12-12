db62da835a78f425dac23bc517c6664379731a1e
SilvanaAlberdi <silvanaalberdi@ni.eus>
2022-12-12 10:54:14 +0100

    Programming

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Coins = 0;
+    [SerializeField]private float speed = 3f;
+    [SerializeField] private float turnSpeed = 15f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Cada vez que haya una colisión entre el Player y un Trigger (en este caso la moneda)
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.name.Contains("Coin"))
+        {
+            Destroy(other.gameObject);
+            Coins += 1;
+        }
     }
 }
